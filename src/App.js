b265d1ee0832576f9186a619873d8e3848d070b2
jpsm83 <jpsm83@hotmail.com>
2021-10-18 12:02:39 +0200

    normal-css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { Route, Switch } from 'react-router';
-import 'tailwindcss'
+import Navbar from './components/Navbar'
 import './App.css';
 import Home from './pages/Home/Home';
 
 export default function App() {
   return (
     <div>
+      <div className="navbar">
+        <Navbar />
+      </div>
       <Switch>
         <Route exact path='/' component={Home} />
       </Switch>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react'
-import Navbar from '../../components/Navbar'
 
 export default class Home extends Component {
     render() {
         return (
             <div>
-                <Navbar />
             </div>
         )
     }
